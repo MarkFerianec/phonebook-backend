22e01b0845e5e009d8ddc9d007cebbb8371efefe
Mark Ferianec <mjf397@gmail.com>
2024-07-09 12:27:28 -0600

    Finish exercise 3.16

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ app.use(express.static('dist'));
 
 const People = require('./models/people');
 
-app.get('/info', (request, response) => {
-  const phonebookEntries = persons.length;
-  const date = new Date();
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
 
-  response.send(
-    `<p>Phonebook has info for ${phonebookEntries} people</p></br><p>${date}</p>`
-  );
-});
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
 
 app.get('/api/persons', (request, response) => {
   People.find({}).then((people) => {
@@ -27,26 +28,25 @@ app.get('/api/persons', (request, response) => {
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  People.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  People.findByIdAndDelete(request.params.id).then((result) => {
-    response.status(204).end();
-  });
+app.delete('/api/persons/:id', (request, response, next) => {
+  People.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
-
-const generateId = () => {
-  return String(Math.floor(Math.random() * 999999));
-};
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
@@ -60,6 +60,8 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson);
   });
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
