460311d9522d7e547f1981afaefe3bf532178016
Mark Ferianec <mjf397@gmail.com>
2024-07-09 11:44:58 -0600

    Finish exercise 3.15

--- index.js
+++ index.js
@@ -12,33 +12,6 @@ app.use(express.static('dist'));
 
 const People = require('./models/people');
 
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-// app.get('/', (request, response) => {
-//   response.send('<h1>Hello World!</h1>');
-// });
-
 app.get('/info', (request, response) => {
   const phonebookEntries = persons.length;
   const date = new Date();
@@ -47,10 +20,6 @@ app.get('/info', (request, response) => {
     `<p>Phonebook has info for ${phonebookEntries} people</p></br><p>${date}</p>`
   );
 });
-
-// app.get('/api/persons', (request, response) => {
-//   response.json(persons);
-// });
 
 app.get('/api/persons', (request, response) => {
   People.find({}).then((people) => {
@@ -70,10 +39,9 @@ app.get('/api/persons/:id', (request, response) => {
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  People.findByIdAndDelete(request.params.id).then((result) => {
+    response.status(204).end();
+  });
 });
 
 const generateId = () => {
@@ -93,7 +61,6 @@ app.post('/api/persons', (request, response) => {
   });
 });
 
-// const PORT = process.env.PORT || 3001;
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
